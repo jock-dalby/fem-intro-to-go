786b5d27b03e8fab686492ba3c9eeb6b2335e0b1
jock-citrus <jock.dalby@citrusad.com>
2021-01-13 08:53:32 +1000

    create examples of fmt's Println, Print and Printf

--- practice.go
+++ practice.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Hello World") // inserts new line
+	// Hello World
+	fmt.Print("Hello Again") // puts on same line
+	fmt.Print("Hello Again")
+	fmt.Printf("Hello a %s time. Total count is %d", "third", 3) // puts on same line. %s for string, %d for number
+	// Hello AgainHelloAgainHello a third time
 }
