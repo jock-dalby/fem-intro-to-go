9ecbf2ce97c900fee997b50bdd2c8b38b05dec59
jock-citrus <jock.dalby@citrusad.com>
2021-01-18 09:44:57 +1000

    complete exercise_3a.go

---
+++ 03_basic_syntax/code/exercise_3a.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	mySentence := "My sentence"
+
+	for index, letter := range mySentence {
+		if index%2 != 0 {
+			fmt.Println("Index:", index, "Letter:", string(letter))
+		}
+	}
+
+	for _, letter := range mySentence { // if index not needed use underscore
+		fmt.Println("Letter:", string(letter))
+	}
+}
--- 03_basic_syntax/code/for.go
+++ 03_basic_syntax/code/for.go
@@ -1,5 +1,6 @@
 package main
 
+/*
 import "fmt"
 
 func main() {
@@ -24,49 +25,47 @@ func main() {
 		fmt.Println("Index:", index, "Letter:", letter)
 	}
 
-	/*
-		Index: 0 Letter: 84
-		Index: 1 Letter: 104
-		Index: 2 Letter: 105
-		Index: 3 Letter: 115
-		Index: 4 Letter: 32
-		Index: 5 Letter: 105
-		Index: 6 Letter: 115
-		Index: 7 Letter: 32
-		Index: 8 Letter: 97
-		Index: 9 Letter: 32
-		Index: 10 Letter: 115
-		Index: 11 Letter: 101
-		Index: 12 Letter: 110
-		Index: 13 Letter: 116
-		Index: 14 Letter: 101
-		Index: 15 Letter: 110
-		Index: 16 Letter: 99
-		Index: 17 Letter: 101
-	*/
+	// Index: 0 Letter: 84
+	// Index: 1 Letter: 104
+	// Index: 2 Letter: 105
+	// Index: 3 Letter: 115
+	// Index: 4 Letter: 32
+	// Index: 5 Letter: 105
+	// Index: 6 Letter: 115
+	// Index: 7 Letter: 32
+	// Index: 8 Letter: 97
+	// Index: 9 Letter: 32
+	// Index: 10 Letter: 115
+	// Index: 11 Letter: 101
+	// Index: 12 Letter: 110
+	// Index: 13 Letter: 116
+	// Index: 14 Letter: 101
+	// Index: 15 Letter: 110
+	// Index: 16 Letter: 99
+	// Index: 17 Letter: 101
 
 	for index, letter := range mySentence {
 		fmt.Println("Index:", index, "Letter:", string(letter))
 	}
 
-	/*
-		Index: 0 Letter: T
-		Index: 1 Letter: h
-		Index: 2 Letter: i
-		Index: 3 Letter: s
-		Index: 4 Letter:
-		Index: 5 Letter: i
-		Index: 6 Letter: s
-		Index: 7 Letter:
-		Index: 8 Letter: a
-		Index: 9 Letter:
-		Index: 10 Letter: s
-		Index: 11 Letter: e
-		Index: 12 Letter: n
-		Index: 13 Letter: t
-		Index: 14 Letter: e
-		Index: 15 Letter: n
-		Index: 16 Letter: c
-		Index: 17 Letter: e
-	*/
+	// Index: 0 Letter: T
+	// Index: 1 Letter: h
+	// Index: 2 Letter: i
+	// Index: 3 Letter: s
+	// Index: 4 Letter:
+	// Index: 5 Letter: i
+	// Index: 6 Letter: s
+	// Index: 7 Letter:
+	// Index: 8 Letter: a
+	// Index: 9 Letter:
+	// Index: 10 Letter: s
+	// Index: 11 Letter: e
+	// Index: 12 Letter: n
+	// Index: 13 Letter: t
+	// Index: 14 Letter: e
+	// Index: 15 Letter: n
+	// Index: 16 Letter: c
+	// Index: 17 Letter: e
+
 }
+*/
