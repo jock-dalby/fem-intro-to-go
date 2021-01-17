68912092fea0cd05d0c8e51dd77c218461f67ef0
jock-citrus <jock.dalby@citrusad.com>
2021-01-18 09:20:19 +1000

    complete ifs.go

--- 03_basic_syntax/code/ifs.go
+++ 03_basic_syntax/code/ifs.go
@@ -1,47 +1,46 @@
-// // Uncomment this entire file
-
 package main
 
-// import (
-// 	"fmt",
-// 	"errors"
-// )
+import (
+	"errors"
+	"fmt"
+)
 
-// func someFunction() error {
-// 	return errors.New("some error")
-// }
+// => Function returns an error of type Error
+func someFunction() error {
+	return errors.New("some error")
+}
 
-// func main() {
+func main() {
 
-// 	var someVar = 9
+	var someVar = 9
 
-// 	if someVar > 10 {
-// 		fmt.Println(someVar)
-// 	}
+	if someVar > 10 { // curly braces required, parentheses are not
+		fmt.Println(someVar)
+	}
 
-// 	// 	// ****************************
+	// ****************************
 
-// 	// if someVar > 100 {
-// 	// 	fmt.Println("Greater than 100")
-// 	// } else if someVar == 100 {
-// 	// 	fmt.Println("Equals 100")
-// 	// } else {
-// 	// 	fmt.Println("Less than 100")
-// 	// }
+	if someVar > 100 {
+		fmt.Println("Greater than 100")
+	} else if someVar == 100 {
+		fmt.Println("Equals 100")
+	} else {
+		fmt.Println("Less than 100")
+	}
 
-// 	// 	// ****************************
-// 	// err := someFunction()
-// 	// // => If this function returns a value,
-// 	// // => it will be an  error of type Error
+	// ****************************
+	err := someFunction()
 
-// 	// 	// ****************************
-// 	// if err != nil {
-// 	//   fmt.Println(err.Error())
-// 	// }
+	// check the variable before executing code
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
-// 	// if err := someFunction(); err != nil {
-// 	//   fmt.Println(err.Error())
-// 	// }
-
-// 	// // End of file curly brace
-// }
+	// assign variable and check the variable before executing code
+	if err := someFunction(); err != nil {
+		// Because err defined within if block, it is only scoped
+		// within the if block, so if want to redeclare anywhere else
+		// we can.
+		fmt.Println(err.Error())
+	}
+}
